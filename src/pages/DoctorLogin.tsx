5d5f99de0ffcbc7d7460b48ec8d5ff1e8068d59a
Vrushabh6454 <vrushabhsalunke4855@gmail.com>
2025-07-29 13:08:11 +0530

    Update DoctorLogin.tsx

--- src/pages/DoctorLogin.tsx
+++ src/pages/DoctorLogin.tsx
@@ -112,7 +112,7 @@ const DoctorDashboard = () => {
 
   const sendAlert = async (alertInfo: any) => {
     try {
-      const response = await fetch("http://localhost:5000/api/alert/send", {
+      const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/alert/send`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
